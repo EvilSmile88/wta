46d6e4617ad223b1ba5446d798e547e8e3ced951
SOFTEQ\apodymov <artyom.podymov@softeq.com>
2018-07-06 20:16:03 +0300

    added player twitc, gear and played tournaments

---
+++ src/js/player-played-tournaments.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper('.swiper-container--played-tournaments', {
+  slidesPerView: 1,
+  spaceBetween: 5,
+  pagination: {
+    clickable: true,
+    dynamicBullets: true,
+    dynamicMainBullets: 1,
+    el: '.swiper-pagination'
+  },
+});
